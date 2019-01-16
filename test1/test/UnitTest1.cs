af0e26d7157534ee3a1fa2515d6c5fa7635ccb92
tatae <tachapong999.gmail.com>
2019-01-16 13:14:39 +0700

    test test1

--- test1/test/UnitTest1.cs
+++ test1/test/UnitTest1.cs
@@ -1,13 +1,20 @@
 using System;
+using interestapi;
 using Xunit;
 
 namespace test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(100,12)]
+        [InlineData(1000,120)]
+        public void Test1(int number, int expected)
         {
+           var sut =  new logic();
+           var result = sut.result(number);
+           Assert.Equal(expected, result);
+       
 
         }
     }
